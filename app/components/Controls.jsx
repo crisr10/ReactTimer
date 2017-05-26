76bc7cf7a0dd48e81db6e2f3756a20c51cdc5980
Cristian Rodriguez <cristian_rocas@hotmail.com>
2017-05-26 13:10:24 -0700

    Add timerStatus to Controls proptypes

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 var Controls = React.createClass({
 	propTypes: {
+		timerStatus: React.PropTypes.string.isRequired,
 		countdownStatus: React.PropTypes.string.isRequired,
 		onStatusChange: React.PropTypes.func.isRequired
 	},
@@ -14,11 +15,11 @@ var Controls = React.createClass({
 		console.log('componentWillReceiveProps', newProps.countdownStatus);
 	},
 	render: function () {
-		var {countdownStatus} = this.props;
+		var {timerStatus, countdownStatus} = this.props;
 		var renderStartStopButton = () => {
-			if (countdownStatus === 'started') {
+			if (countdownStatus === 'started' || timerStatus === 'started') {
 				return <button className='button secondary' onClick={this.onStatusChange('paused')}>Pause</button>
-			} else if (countdownStatus === 'paused') {
+			} else if (countdownStatus === 'paused' || timerStatus === 'paused') {
 				return <button className='button primary' onClick={this.onStatusChange('started')}>Start</button>
 			}
 		}
